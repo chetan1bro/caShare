d453887ba53b611b7ba298a0fe6f32f68581c13c
chetan1bro <87486003+chetan1bro@users.noreply.github.com>
2022-08-27 15:34:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 app.use(express.static('public'));
 app.use(express.json());
 
